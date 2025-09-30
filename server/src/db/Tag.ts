bbccde4af73aeadfb02a7e7680400d8f38f27f51
Codersintu <skmahto9199@gmail.com>
2025-09-30 10:05:46 +0530

    Remove server folder from repo

--- server/src/db/Tag.ts
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-const tagSchema = new mongoose.Schema({
-  title: { type: String, required: true, unique: true }
-});
-
-const Tag = mongoose.model('Tag', tagSchema);
-export default Tag;
