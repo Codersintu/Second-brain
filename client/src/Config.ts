7721fe211be3d9004e35a525af94fa9ad277ad64
Codersintu <skmahto9199@gmail.com>
2025-10-11 21:53:06 +0530

    added

--- client/src/Config.ts
+++ client/src/Config.ts
@@ -1,1 +1,1 @@
-export const BACKEND_URL="https://second-brain-smoky-beta.vercel.app"
+export const BACKEND_URL="https://server-7nl8.vercel.app"
--- server/src/utility.ts
+++
@@ -1,9 +0,0 @@
-export function random(len:number){
-    let option="erngjrtntrijoefmkfvgkdefrf";
-    let length=option.length;
-    let ans=""
-    for (let i = 0; i < len; i++) {
-        ans+=option[Math.floor((Math.random() * length))]
-    }
-    return ans;
-}
