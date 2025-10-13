7ff99164b5b662256c2d75fd69c0f97e3556029d
Codersintu <skmahto9199@gmail.com>
2025-10-13 23:24:02 +0530

    added logout btn

--- client/src/Sidebar.tsx
+++ client/src/Sidebar.tsx
@@ -1,13 +1,19 @@
 import { useRecoilState} from "recoil"
 import { filterAtom } from "./Atom"
+import { useNavigate } from "react-router-dom"
 
 
 function Sidebar() {
+  const navigate=useNavigate()
   const [FilterContent,setFilter]=useRecoilState<string>(filterAtom)
-
+ function Logouthandler() {
+    localStorage.removeItem("token")
+    localStorage.removeItem("cachedContent")
+    navigate("/auth")
+ }
   return (
     <div className="md:w-64 w-16 h-screen shadow-md bg-white top-0 sticky z-10">
-      <div className="flex flex-col md:px-5 px-2 py-4 gap-10">
+      <div className="flex flex-col  md:px-5 px-2 py-4 gap-10">
         <div className="flex gap-4 items-center">
             <img className="w-10" src="../brain.png" alt="" />
             <h1 className="text-2xl hidden md:block text-blue-600 font-semibold">Second Brain</h1>
@@ -34,6 +40,10 @@ function Sidebar() {
                 <p className="hidden md:block">Tags</p>
             </div>
          </div>
+         <div onClick={Logouthandler} className="flex items-center hover:bg-gray-300 p-1 rounded-md cursor-pointer font-semibold gap-3 bottom-0 mt-20  ">
+                <img className="w-8" src="../switch.png" alt="" />
+                <p className="hidden md:block">LogOut</p>
+            </div>
       </div>
     </div>
   )
