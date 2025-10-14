410fe48d120202aed1316a3515bac6d1744262f2
Codersintu <skmahto9199@gmail.com>
2025-10-14 08:03:45 +0530

    emoji

--- client/src/Content.tsx
+++ client/src/Content.tsx
@@ -15,7 +15,7 @@ const {shareBtn}=useFetch(`${BACKEND_URL}/api/v1/brain/share`)
     <>
     <div className="md:p-10 flex-1 h-screen bg-cyan-50 font-serif gap-10 overflow-auto relative z-0 group">  
      <div className="flex justify-between items-center">
-      <h1 className="md:text-2xl text-xl font-semibold">All Memory😁</h1>
+      <h1 className="md:text-2xl text-xl font-semibold">All Memory😍</h1>
       <div className="flex gap-5 items-center">
         <div className="flex items-center gap-2 bg-cyan-100 rounded-lg md:p-2 p-1 cursor-pointer" onClick={shareBtn}>
           <ShareIcon/>
